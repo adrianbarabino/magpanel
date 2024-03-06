3840f70fa302862dcfafb4001638d0c33e62acef
Adrian Barabino <adrian.barabino@gmail.com>
2024-03-06 03:21:37 -0300

    Fix dataset

--- src/Datasets.svelte
+++ src/Datasets.svelte
@@ -20,25 +20,25 @@
         <div class="features">
             <div class="container">
                 <div class="row">
-                  <a href="javascript:void(0);" on:click={() => navigateTo('/logs')}  class="col-4 bg-primary text-white text-center py-3">
+                  <a href="javascript:void(0);" on:click={() => broteNavigate('/logs')}  class="col-4 bg-primary text-white text-center py-3">
                     
                       <span class="fa fa-clipboard-list"></span> Registros
                     </a>
                   
-                  <a href="javascript:void(0);" on:click={() => navigateTo('/users')}  class="col-8 bg-warning text-black text-center py-3">
+                  <a href="javascript:void(0);" on:click={() => broteNavigate('/users')}  class="col-8 bg-warning text-black text-center py-3">
                     
                       <span class="fa fa-user"></span> Usuarios
                     </a>
-                    <a href="javascript:void(0);" on:click={() => navigateTo('/project-statuses')}  class="col-4 bg-info text-white text-center py-3">
+                    <a href="javascript:void(0);" on:click={() => broteNavigate('/project-statuses')}  class="col-4 bg-info text-white text-center py-3">
                         
                         <span class="fa fa-tag"></span> Estados de Proyectos
                       </a>
-                  <a href="javascript:void(0);" on:click={() => navigateTo('/categories')}  class="col-5 bg-success text-white text-center py-3">
+                  <a href="javascript:void(0);" on:click={() => broteNavigate('/categories')}  class="col-5 bg-success text-white text-center py-3">
                     
                       <span class="fa fa-tags"></span> Categorías
                     </a>
 
-                    <a href="javascript:void(0);" on:click={() => navigateTo('/locations')}  class="col-3 bg-danger text-white text-center py-3">
+                    <a href="javascript:void(0);" on:click={() => broteNavigate('/locations')}  class="col-3 bg-danger text-white text-center py-3">
                       
                         <span class="fa fa-map"></span> Ubicaciones
                       </a>
