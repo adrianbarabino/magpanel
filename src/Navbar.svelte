4b95dfd71d1fdbc22ebf0495ea2de728ee42050a
Adrian Barabino <adrian.barabino@gmail.com>
2024-03-19 03:15:25 -0300

    Update styles and add Bootstrap CSS

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,96 +1,62 @@
 <script>
   import { broteNavigate } from './utils/navigation';
   import { username } from './routes';
-	import SwitchButton from './SwitchButton.svelte'
+  import SwitchButton from './SwitchButton.svelte'
 
-  // generar distintos greetings dependiendo de la hora del día, para que el saludo sea más amigable, varias opciones
   let greeting = '';
-  let date = new Date();
-  let hour = date.getHours();
-  let allGreetings = [];
+  const date = new Date();
+  const hour = date.getHours();
+  let allGreetings = [
+      `¡Hola, ${username}!`,
+      `¡Excelente verte, ${username}!`
+  ];
   
-if (hour < 12) {
-    allGreetings = [
-        `¡Buen día, ${username}!`,
-        `¡Hola, ${username}!`,
-        `¿Cómo amaneciste, ${username}?`,
-        `¡Genial verte, ${username}!`,
-        `¡Vamos arrancando, ${username}!`
-    ];
-} else if (hour < 18) {
-    allGreetings = [
-        `¡Buenas, ${username}!`,
-        `¡Hola, ${username}!`,
-        `¿Cómo va la tarde, ${username}?`,
-        `¡Excelente verte, ${username}!`,
-        `¡A seguir, ${username}!`
-    ];
-} else {
-    allGreetings = [
-        `¡Buenas noches, ${username}!`,
-        `¡Hola, ${username}!`,
-        `¿Cómo te trata la noche, ${username}?`,
-        `¡Bueno verte, ${username}!`,
-        `¡A relajarse, ${username}!`,
-        `¡A descansar, ${username}!`
-    ];
-}
-greeting = allGreetings[Math.floor(Math.random() * allGreetings.length)];
-
+  switch (true) {
+      case hour < 6:
+      allGreetings.push(`¡Buenas noches, ${username}!`, `¿Cómo te trata la noche, ${username}?`, `¡A relajarse, ${username}!`, `¡A descansar, ${username}!`);
+          break;
+      case hour < 12:
+          allGreetings.push(`¡Buen día, ${username}!`, `¿Cómo amaneciste, ${username}?`, `¡Vamos arrancando, ${username}!`);
+          break;
+      case hour < 18:
+          allGreetings.push(`¡Buenas, ${username}!`, `¿Cómo va la tarde, ${username}?`, `¡A seguir, ${username}!`);
+          break;
+      default:
+          allGreetings.push(`¡Buenas noches, ${username}!`, `¿Cómo te trata la noche, ${username}?`, `¡A relajarse, ${username}!`, `¡A descansar, ${username}!`);
+  }
+  greeting = allGreetings[Math.floor(Math.random() * allGreetings.length)];
 </script>
 
-        <nav class="navbar navbar-expand-lg fixed-top  navbar-dark bg-dark">
-          <div class="container-fluid">
-           <a href="javascript:void(0);" on:click={(event) =>  broteNavigate('/')} class="logo">
-                <figure>
-                    <img src="https://mag-servicios.com/wp-content/uploads/2019/10/04-Blanco-800x362.png" height="40" alt="">
-                </figure>
-            </a>
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+  <div class="container-fluid">
+    <!-- Botón para versión móvil y elementos alineados a la izquierda -->
+    <button class="navbar-toggler" type="button" id="sidebarCollapseTop" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#" on:click={(event) => broteNavigate('/home')}>Panel de Control</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" on:click={(event) => broteNavigate('/create-project')}>Crear Proyecto</a>
+        </li>
+      </ul>
+    </div>
 
-            <button class="btn btn-dark d-inline-block d-lg-none ms-auto" type="button" id="sidebarCollapseTop" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fa fa-bars"></i>
-            </button>
+    <!-- Logo centralizado para todas las pantallas -->
+    <a class="navbar-brand mx-auto" href="#" on:click={(event) => broteNavigate('/home')}>
+      <img src="https://mag-servicios.com/wp-content/uploads/2019/10/04-Blanco-800x362.png" height="40" alt="Logo MAG">
+    </a>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a href="javascript:void(0);" on:click={(event) =>  broteNavigate('/')} class="nav-link">Panel de Control</a>
-                </li>
-                <li class="nav-item">
-                    <a href="javascript:void(0);" on:click={(event) =>  broteNavigate('/create-client')} class="nav-link">Crear cliente</a>
-                </li>
-                <li class="nav-item">
-                    <a href="javascript:void(0);" on:click={(event) =>  broteNavigate('/create-contact')} class="nav-link">Crear contacto</a>
-                </li>
-                <li class="nav-item">
-                    <a href="javascript:void(0);" on:click={(event) =>  broteNavigate('/create-provider')} class="nav-link">Crear proveedor</a>
-                </li>
-                <li class="nav-item">
-                    <a href="javascript:void(0);" on:click={(event) =>  broteNavigate('/create-project')} class="nav-link">Crear proyecto</a>
-                </li>
-                
-              </ul>
-             
-            </div>
-            
-            {#if username}
-            <div class="greeting text-white">
-              <span>{greeting}
-                
-              <a href="javascript:void(0);" on:click={(event) =>  broteNavigate('/logout')} class="text-white 
-                  badge bg-danger">Salir</a>
-              </span>
-            </div>  
-            
-            <div>
-              <SwitchButton></SwitchButton>
-            </div>
-            {/if}
-          </div>
-        </nav>
-<style>
-body{
-	padding:0;
-}
-</style>
+    <!-- Contenido alineado a la derecha -->
+    {#if username}
+      <div class="navbar-text d-none d-lg-block ml-auto">
+        <span class="text-white mx-2">{greeting}</span>
+        <a class="btn btn-danger text-white btn-sm" href="#" on:click={(event) => broteNavigate('/logout')}>Salir</a>
+        <SwitchButton></SwitchButton>
+      </div>
+    {/if}
+  </div>
+</nav>
